e25ce38fbfb29cbc0d295b57b315ccf106d08811
Sk1palong <cchs33@gmail.com>
2024-01-08 12:30:09 +0900

    [#4] feat : Course 목록 조회 기능 추가

--- src/main/java/com/b6/mypaldotrip/domain/course/controller/CourseController.java
+++ src/main/java/com/b6/mypaldotrip/domain/course/controller/CourseController.java
@@ -1,14 +1,17 @@
 package com.b6.mypaldotrip.domain.course.controller;
 
 import com.b6.mypaldotrip.domain.course.controller.dto.requeset.CourseSaveReq;
+import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseListRes;
 import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseSaveRes;
 import com.b6.mypaldotrip.domain.course.service.CourseService;
 import com.b6.mypaldotrip.global.common.GlobalResultCode;
 import com.b6.mypaldotrip.global.config.VersionConfig;
 import com.b6.mypaldotrip.global.response.RestResponse;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,13 @@ public class CourseController {
         CourseSaveRes res = courseService.saveCourse(req);
         return RestResponse.success(res, GlobalResultCode.CREATED, versionConfig.getVersion())
             .toResponseEntity();
+    }
+
+    @GetMapping
+    public ResponseEntity<RestResponse<List<CourseListRes>>> getCourse() {
+         List<CourseListRes> res = courseService.getCourseList();
+         return RestResponse.success(res, GlobalResultCode.SUCCESS, versionConfig.getVersion())
+             .toResponseEntity();
     }
 
 }
--- src/main/java/com/b6/mypaldotrip/domain/course/service/CourseService.java
+++ src/main/java/com/b6/mypaldotrip/domain/course/service/CourseService.java
@@ -1,9 +1,11 @@
 package com.b6.mypaldotrip.domain.course.service;
 
 import com.b6.mypaldotrip.domain.course.controller.dto.requeset.CourseSaveReq;
+import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseListRes;
 import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseSaveRes;
 import com.b6.mypaldotrip.domain.course.store.entity.CourseEntity;
 import com.b6.mypaldotrip.domain.course.store.repository.CourseRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,17 @@ public class CourseService {
             .title(course.getTitle())
             .content(course.getContent())
             .build();
+
+        return res;
+    }
+
+    public List<CourseListRes> getCourseList() {
+        List<CourseListRes> res = courseRepository.findAll().stream()
+            .map(c -> CourseListRes.builder()
+                .title(c.getTitle())
+                .content(c.getContent())
+                .build()
+            ).toList();
 
         return res;
     }
