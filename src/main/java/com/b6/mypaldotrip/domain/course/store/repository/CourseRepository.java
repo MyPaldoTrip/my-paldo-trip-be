72c796598944f7bbbcb7b84c25d7a2044d81b27e
Sk1palong <cchs33@gmail.com>
2024-01-09 16:09:09 +0900

    [#19] feat : QueryDSL 관련 repository 설정

--- src/main/java/com/b6/mypaldotrip/domain/course/store/repository/CourseRepository.java
+++ src/main/java/com/b6/mypaldotrip/domain/course/store/repository/CourseRepository.java
@@ -2,5 +2,8 @@ package com.b6.mypaldotrip.domain.course.store.repository;
 
 import com.b6.mypaldotrip.domain.course.store.entity.CourseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CourseRepository extends JpaRepository<CourseEntity, Long> {}
+@Repository
+public interface CourseRepository
+        extends JpaRepository<CourseEntity, Long>, CustomCourseRepository {}
