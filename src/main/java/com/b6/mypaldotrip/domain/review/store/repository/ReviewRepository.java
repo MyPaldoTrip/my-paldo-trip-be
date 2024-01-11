8e5bde3ae97e25f236f80dea412a5321a58b5334
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-12 00:04:14 +0900

    [#31] fix : 쿼리 메서드 반환 타입 변경

--- src/main/java/com/b6/mypaldotrip/domain/review/store/repository/ReviewRepository.java
+++ src/main/java/com/b6/mypaldotrip/domain/review/store/repository/ReviewRepository.java
@@ -2,9 +2,8 @@ package com.b6.mypaldotrip.domain.review.store.repository;
 
 import com.b6.mypaldotrip.domain.review.store.entity.ReviewEntity;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
-    Optional<List<ReviewEntity>> findByTrip_TripId(Long tripId);
+    List<ReviewEntity> findByTrip_TripId(Long tripId);
 }
