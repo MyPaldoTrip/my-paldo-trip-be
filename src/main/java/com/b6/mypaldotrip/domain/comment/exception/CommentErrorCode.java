62a0c851b65c4172aa0f55938acb2b7e273d098a
Sk1palong <cchs33@gmail.com>
2024-01-13 01:38:48 +0900

    [#61] feat : Comment 전체조회, 팔로우한 유저별 페이징 구현

--- src/main/java/com/b6/mypaldotrip/domain/comment/exception/CommentErrorCode.java
+++ src/main/java/com/b6/mypaldotrip/domain/comment/exception/CommentErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CommentErrorCode implements ResultCode {
+    WRONG_COMMENT_SORT(HttpStatus.BAD_REQUEST, "잘못된 댓글 정렬 방식 입니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다");
 
     private final HttpStatus httpStatus;
--- src/main/java/com/b6/mypaldotrip/domain/comment/store/repository/CommentRepository.java
+++ src/main/java/com/b6/mypaldotrip/domain/comment/store/repository/CommentRepository.java
@@ -2,5 +2,8 @@ package com.b6.mypaldotrip.domain.comment.store.repository;
 
 import com.b6.mypaldotrip.domain.comment.store.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {}
+@Repository
+public interface CommentRepository
+        extends JpaRepository<CommentEntity, Long>, CustomCommentRepository {}
