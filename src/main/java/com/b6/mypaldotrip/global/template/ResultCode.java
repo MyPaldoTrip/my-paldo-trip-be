2117250831a1c80087581277e32c4d0038dd3c9e
asdvsvs <gag973000@gmail.com>
2024-01-12 11:29:30 +0900

    [#1] fix : 사용하지 않는 메서드 제거

--- src/main/java/com/b6/mypaldotrip/global/template/ResultCode.java
+++ src/main/java/com/b6/mypaldotrip/global/template/ResultCode.java
@@ -3,9 +3,6 @@ package com.b6.mypaldotrip.global.template;
 import org.springframework.http.HttpStatus;
 
 public interface ResultCode {
-
-    String name();
-
     HttpStatus getHttpStatus();
 
     String getMessage();
