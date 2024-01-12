efa8a0df80c493418ea0172d21da5fb1f543543e
asdvsvs <gag973000@gmail.com>
2024-01-12 12:08:57 +0900

    fix : spotless

--- src/main/java/com/b6/mypaldotrip/domain/user/store/entity/UserRole.java
+++ src/main/java/com/b6/mypaldotrip/domain/user/store/entity/UserRole.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-    ROLE_ADMIN("ROLE_ADMIN","관리자"),
-    ROLE_OPERATOR("ROLE_OPERATOR","운영자"),
-    ROLE_USER("ROLE_USER","유저");
+    ROLE_ADMIN("ROLE_ADMIN", "관리자"),
+    ROLE_OPERATOR("ROLE_OPERATOR", "운영자"),
+    ROLE_USER("ROLE_USER", "유저");
 
     private final String code;
     private final String value;
