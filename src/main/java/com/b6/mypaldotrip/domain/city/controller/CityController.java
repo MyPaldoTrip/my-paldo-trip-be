a4c899027fe0e77a1dd92052a2bf93e98acec6b2
yoooooungwoon <kim0310hwang@gmail.com>
2024-01-16 21:01:05 +0900

    [#76]feat : 파일 업로드, 수정, 삭제 기능

--- src/main/java/com/b6/mypaldotrip/domain/city/controller/CityController.java
+++ src/main/java/com/b6/mypaldotrip/domain/city/controller/CityController.java
@@ -8,10 +8,13 @@ import com.b6.mypaldotrip.domain.city.controller.dto.response.CityDeleteRes;
 import com.b6.mypaldotrip.domain.city.controller.dto.response.CityListRes;
 import com.b6.mypaldotrip.domain.city.controller.dto.response.CityUpdateRes;
 import com.b6.mypaldotrip.domain.city.controller.dto.response.ProvinceListRes;
+import com.b6.mypaldotrip.domain.city.s3.dto.S3Res;
+import com.b6.mypaldotrip.domain.city.s3.service.S3Service;
 import com.b6.mypaldotrip.domain.city.service.CityService;
 import com.b6.mypaldotrip.global.common.GlobalResultCode;
 import com.b6.mypaldotrip.global.config.VersionConfig;
 import com.b6.mypaldotrip.global.response.RestResponse;
+import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +25,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -31,6 +36,7 @@ public class CityController {
 
     private final CityService cityService;
     private final VersionConfig versionConfig;
+    private final S3Service s3Service;
 
     @PostMapping // 생성
     public ResponseEntity<RestResponse<CityCreateRes>> createCity(
@@ -85,6 +91,31 @@ public class CityController {
             @RequestBody ProvinceListReq req) {
         List<ProvinceListRes> res = cityService.getProvinceListInfoSort(req);
 
+        return RestResponse.success(res, GlobalResultCode.SUCCESS, versionConfig.getVersion())
+                .toResponseEntity();
+    }
+
+    @PostMapping("/{cityId}/files") // 파일 생성
+    public ResponseEntity<RestResponse<S3Res>> saveFile(
+            @PathVariable Long cityId, @RequestPart(value = "file") MultipartFile multipartFile)
+            throws IOException {
+        S3Res res = s3Service.saveFile(cityId, multipartFile);
+        return RestResponse.success(res, GlobalResultCode.SUCCESS, versionConfig.getVersion())
+                .toResponseEntity();
+    }
+
+    @PutMapping("/files/{fileId}") // 파일 수정
+    public ResponseEntity<RestResponse<S3Res>> updateFile(
+            @PathVariable Long fileId, @RequestPart(value = "file") MultipartFile multipartFile)
+            throws IOException {
+        S3Res res = s3Service.updateFile(fileId, multipartFile);
+        return RestResponse.success(res, GlobalResultCode.SUCCESS, versionConfig.getVersion())
+                .toResponseEntity();
+    }
+
+    @DeleteMapping("/files/{fileId}") // 파일 삭제
+    public ResponseEntity<RestResponse<S3Res>> deleteFile(@PathVariable Long fileId) {
+        S3Res res = s3Service.deleteFile(fileId);
         return RestResponse.success(res, GlobalResultCode.SUCCESS, versionConfig.getVersion())
                 .toResponseEntity();
     }
