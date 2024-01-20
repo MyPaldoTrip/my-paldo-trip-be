80d72b681115d1d74048683fd3fe91feaf0e1e1c
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-21 03:50:22 +0900

    [#83] fix : WebSecurityConfig 모든 요청 허용

--- src/main/java/com/b6/mypaldotrip/global/config/WebSecurityConfig.java
+++ src/main/java/com/b6/mypaldotrip/global/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
