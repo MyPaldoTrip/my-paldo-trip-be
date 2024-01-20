8de6f0485342e8903232bc669a88f2e415bfae90
Sk1palong <cchs33@gmail.com>
2024-01-21 01:46:30 +0900

    [#79] feat : Course 요청, 반환값 변경

--- src/main/java/com/b6/mypaldotrip/domain/course/controller/dto/response/CourseSaveRes.java
+++ src/main/java/com/b6/mypaldotrip/domain/course/controller/dto/response/CourseSaveRes.java
@@ -3,4 +3,4 @@ package com.b6.mypaldotrip.domain.course.controller.dto.response;
 import lombok.Builder;
 
 @Builder
-public record CourseSaveRes(String title, String content) {}
+public record CourseSaveRes(Long courseId, String title, String content) {}
