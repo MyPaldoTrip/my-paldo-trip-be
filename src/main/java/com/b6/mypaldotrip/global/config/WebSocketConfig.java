58a88f9eaa5a26ccce9556f8e08d072c1e04e2b5
Mcgeolypazun <taratula0504@gmail.com>
2024-01-24 16:04:05 +0900

    feat: WebSocketConfig  -  .setAllowedOriginPatterns("*") 코드 추가
    - Spring Security 설정에서 Cross-Origin Resource Sharing(CORS) 정책을 설정하는 데 사용됩니다. 이 설정은 브라우저가 다른 도메인의 리소스에 접근할 수 있도록 허용합니다.
    - chat.html Content-type 추가

--- src/main/java/com/b6/mypaldotrip/global/config/WebSocketConfig.java
+++ src/main/java/com/b6/mypaldotrip/global/config/WebSocketConfig.java
@@ -25,7 +25,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+            .setAllowedOriginPatterns("*")
+            .withSockJS();
     }
 
     @Override
