f2f9b0c2fcf286f5e89ad9126cfda0d976d242c9
Sk1palong <cchs33@gmail.com>
2024-01-24 09:59:10 +0900

    [#61] feat : Comment 작성자 레벨과 마지막 수정 시간 반환값에 추가

--- src/main/java/com/b6/mypaldotrip/domain/comment/controller/dto/response/CommentListRes.java
+++ src/main/java/com/b6/mypaldotrip/domain/comment/controller/dto/response/CommentListRes.java
@@ -1,6 +1,13 @@
 package com.b6.mypaldotrip.domain.comment.controller.dto.response;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
-public record CommentListRes(Long commentId, String username, String content, int totalPage) {}
+public record CommentListRes(
+        Long commentId,
+        String username,
+        String content,
+        LocalDateTime modifiedAt,
+        Long level,
+        int totalPage) {}
