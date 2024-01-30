cad3665970cf4e5761e2b0307b68d2c2f82d7b70
asdvsvs <gag973000@gmail.com>
2024-01-31 03:26:46 +0900

    [#135] test : 회원탈퇴 서비스 테스트

--- src/test/java/com/b6/mypaldotrip/domain/user/service/UserServiceTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/user/service/UserServiceTest.java
@@ -4,16 +4,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.b6.mypaldotrip.domain.user.CommonTest;
 import com.b6.mypaldotrip.domain.user.controller.dto.request.UserSignUpReq;
+import com.b6.mypaldotrip.domain.user.controller.dto.response.UserDeleteRes;
 import com.b6.mypaldotrip.domain.user.controller.dto.response.UserSignUpRes;
 import com.b6.mypaldotrip.domain.user.exception.UserErrorCode;
 import com.b6.mypaldotrip.domain.user.store.repository.UserRepository;
+import com.b6.mypaldotrip.global.common.GlobalResultCode;
 import com.b6.mypaldotrip.global.common.S3Provider;
 import com.b6.mypaldotrip.global.exception.GlobalException;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -72,6 +76,38 @@ class UserServiceTest implements CommonTest {
 
             // then
             assertThat(exception.getResultCode()).isEqualTo(UserErrorCode.BEFORE_EMAIL_VALIDATION);
+        }
+    }
+
+    @Nested
+    @DisplayName("회원탈퇴 테스트")
+    class 회원탈퇴{
+        @Test
+        @DisplayName("회원탈퇴 테스트 성공 - 파일 있음")
+        void 회원탈퇴1 (){
+            //given
+            String message = "유저, 유저파일 삭제";
+            given(userRepository.findById(TEST_USERID)).willReturn(Optional.ofNullable(TEST_USER));
+
+            //when
+            UserDeleteRes res = userService.deleteUser(TEST_USERID);
+
+            //then
+            assertThat(res.message()).isEqualTo(message);
+        }
+        @Test
+        @DisplayName("회원탈퇴 테스트 성공 - 파일 없음")
+        void 회원탈퇴2 (){
+            //given
+            String message = "유저 삭제, 삭제할 파일 없음";
+            given(userRepository.findById(TEST_USERID)).willReturn(Optional.ofNullable(TEST_USER));
+
+            //when
+            doThrow(new GlobalException(GlobalResultCode.NOT_FOUND_FILE)).when(s3Provider).deleteFile(TEST_USER);
+            UserDeleteRes res = userService.deleteUser(TEST_USERID);
+
+            //then
+            assertThat(res.message()).isEqualTo(message);
         }
     }
 }
