99ec23dfe413ea6beadb5cb45c79b3cfe991174e
Sk1palong <cchs33@gmail.com>
2024-02-01 01:48:55 +0900

    [#142] test : Course requestDto에 @Builder 추가

--- src/main/java/com/b6/mypaldotrip/domain/comment/controller/dto/request/CommentSaveReq.java
+++ src/main/java/com/b6/mypaldotrip/domain/comment/controller/dto/request/CommentSaveReq.java
@@ -1,5 +1,7 @@
 package com.b6.mypaldotrip.domain.comment.controller.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record CommentSaveReq(@NotNull(message = "content가 비었습니다.") String content) {}
