dd129cdf7e57ac65fb1bd77b37a639af485dfdc7
asdvsvs <gag973000@gmail.com>
2024-02-01 04:52:17 +0900

    [#150] test : 신청서목록 조회 컨트롤러 테스트

--- src/test/java/com/b6/mypaldotrip/domain/user/ApplicationCommonTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/user/ApplicationCommonTest.java
@@ -4,4 +4,6 @@ public interface ApplicationCommonTest {
 
     String TEST_TITLE = "title";
     String TEST_CONTENT = "content";
+    Long TEST_APPLICATION_ID = 1L;
+    Boolean DEFAULT_VERIFIED = false;
 }
--- src/test/java/com/b6/mypaldotrip/domain/user/controller/ApplicationControllerTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/user/controller/ApplicationControllerTest.java
@@ -5,14 +5,17 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.b6.mypaldotrip.domain.user.ApplicationCommonTest;
 import com.b6.mypaldotrip.domain.user.CommonControllerTest;
 import com.b6.mypaldotrip.domain.user.controller.dto.request.ApplicationSubmitReq;
+import com.b6.mypaldotrip.domain.user.controller.dto.response.ApplicationGetListRes;
 import com.b6.mypaldotrip.domain.user.controller.dto.response.ApplicationSubmitRes;
 import com.b6.mypaldotrip.domain.user.service.ApplicationService;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -65,5 +68,24 @@ class ApplicationControllerTest extends CommonControllerTest implements Applicat
             //then
             actions.andExpect(status().isBadRequest());
         }
+    }
+
+    @Test
+    @DisplayName("신청서 목록 조회 테스트 성공")
+    void 신청서목록조회 () throws Exception {
+        //given
+        ApplicationGetListRes res = ApplicationGetListRes.builder().applicationId(TEST_APPLICATION_ID)
+            .email(TEST_EMAIL).username(TEST_USERNAME).title(TEST_TITLE).verified(DEFAULT_VERIFIED).build();
+        given(applicationService.getList()).willReturn(List.of(res));
+        //when
+        ResultActions actions = mockMvc.perform(
+            get("/api/" + versionConfig.getVersion() + "/users/application"));
+        //then
+        actions.andExpect(status().isOk())
+            .andDo(
+                document(
+                    "user/application-getList",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint())));
     }
 }
