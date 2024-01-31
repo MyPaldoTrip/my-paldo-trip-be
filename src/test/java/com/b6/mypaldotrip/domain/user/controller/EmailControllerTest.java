ff17ed3a448152fa52febe7608872c1b3ecc3859
asdvsvs <gag973000@gmail.com>
2024-02-01 04:21:50 +0900

    [#149] test : 이메일발송 컨트롤러 테스트

---
+++ src/test/java/com/b6/mypaldotrip/domain/user/controller/EmailControllerTest.java
@@ -0,0 +1,69 @@
+package com.b6.mypaldotrip.domain.user.controller;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.b6.mypaldotrip.domain.user.CommonControllerTest;
+import com.b6.mypaldotrip.domain.user.controller.dto.request.EmailSendReq;
+import com.b6.mypaldotrip.domain.user.controller.dto.response.EmailSendRes;
+import com.b6.mypaldotrip.domain.user.service.EmailService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+
+@WebMvcTest(controllers = {EmailController.class})
+@MockBean(JpaMetamodelMappingContext.class)
+class EmailControllerTest extends CommonControllerTest {
+
+    @MockBean private EmailService emailService;
+
+    @Nested
+    @DisplayName("이메일 발송 테스트")
+    class 이메일발송{
+        @Test
+        @DisplayName("이메일 발송 테스트 성공")
+        void 이메일발송1 () throws Exception {
+            //given
+            EmailSendReq req = EmailSendReq.builder().email(TEST_EMAIL).build();
+            EmailSendRes res = EmailSendRes.builder().recipientEmail(TEST_EMAIL).message("인증 코드 발송").build();
+            given(emailService.sendEmail(req)).willReturn(res);
+
+            //when
+            ResultActions actions = mockMvc.perform(
+                post("/api/" + versionConfig.getVersion() + "/users/email")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(req)));
+
+            //then
+            actions.andExpect(status().isOk())
+                .andDo(
+                    document(
+                        "user/email-send",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint())));
+        }
+        @Test
+        @DisplayName("이메일 발송 테스트 실패")
+        void 이메일발송2 () throws Exception {
+            //given
+            EmailSendReq req = EmailSendReq.builder().email("test").build();
+            //when
+            ResultActions actions = mockMvc.perform(
+                post("/api/" + versionConfig.getVersion() + "/users/email")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(req)));
+            //then
+            actions.andExpect(status().isBadRequest());
+        }
+    }
+}
