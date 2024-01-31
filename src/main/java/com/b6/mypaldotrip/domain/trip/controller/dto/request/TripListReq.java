6eb5cb382d0851dc414f8313e6c7e528b4c7f50f
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-31 20:12:14 +0900

    fix : TripListReq에 @Builder 추가

--- src/main/java/com/b6/mypaldotrip/domain/trip/controller/dto/request/TripListReq.java
+++ src/main/java/com/b6/mypaldotrip/domain/trip/controller/dto/request/TripListReq.java
@@ -2,5 +2,7 @@ package com.b6.mypaldotrip.domain.trip.controller.dto.request;
 
 import com.b6.mypaldotrip.domain.trip.store.entity.Category;
 import com.b6.mypaldotrip.domain.trip.store.entity.TripSort;
+import lombok.Builder;
 
+@Builder
 public record TripListReq(String cityName, Category category, TripSort tripSort, int page) {}
