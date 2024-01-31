031b714d0b8b97c4b84ff37b723964aa6d9649db
jimyung0418 <wjdwlaud0418@naver.com>
2024-01-31 20:14:44 +0900

    [#139] test : 여행정보 목록 조회 테스트 코드

--- src/test/java/com/b6/mypaldotrip/domain/trip/controller/TripControllerTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/trip/controller/TripControllerTest.java
@@ -2,7 +2,9 @@ package com.b6.mypaldotrip.domain.trip.controller;
 
 import com.b6.mypaldotrip.domain.trip.TripTestUtils;
 import com.b6.mypaldotrip.domain.trip.controller.dto.request.TripCreateReq;
+import com.b6.mypaldotrip.domain.trip.controller.dto.request.TripListReq;
 import com.b6.mypaldotrip.domain.trip.controller.dto.response.TripCreateRes;
+import com.b6.mypaldotrip.domain.trip.controller.dto.response.TripListRes;
 import com.b6.mypaldotrip.domain.trip.service.TripService;
 import com.b6.mypaldotrip.domain.trip.store.entity.Category;
 import com.b6.mypaldotrip.global.security.UserDetailsImpl;
@@ -17,7 +19,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -25,6 +29,7 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = {TripController.class})
@@ -125,4 +130,34 @@ class TripControllerTest extends TripTestUtils {
             actions.andExpect(status().isBadRequest());
         }
     }
+
+    @Nested
+    @DisplayName("여행정보 목록 조회 테스트")
+    class 여행정보_목록조회 {
+
+        @Test
+        @DisplayName("여행정보 목록 조회 성공")
+        void 여행정보_목록조회() throws Exception {
+            // given
+            TripListReq req = TripListReq.builder().build();
+            List<TripListRes> res = Arrays.asList(
+                    new TripListRes(1L, TEST_CITY_NAME, Category.ATTRACTION, TEST_TRIP_NAME, 5.0, 100, List.of("url1", "url2"))
+            );
+            given(tripService.getTripList(any(TripListReq.class))).willReturn(res);
+
+            // when
+            ResultActions actions = mockMvc.perform(
+                    post("/api/" + versionConfig.getVersion() + "/trips/lists")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(req))
+            );
+
+            // then
+            actions.andExpect(status().isOk())
+                    .andDo(document("trip/getTripList",
+                            preprocessRequest(prettyPrint()),
+                            preprocessResponse(prettyPrint())));
+        }
+    }
+
 }
