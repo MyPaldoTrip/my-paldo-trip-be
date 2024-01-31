29d9d99b2c8c4cf2504aa5295213d318c70eb4e6
asdvsvs <gag973000@gmail.com>
2024-02-01 07:06:45 +0900

    [#153] test : 코드검증 이메일유틸 테스트

--- src/test/java/com/b6/mypaldotrip/domain/user/service/EmailUtilTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/user/service/EmailUtilTest.java
@@ -1,14 +1,19 @@
 package com.b6.mypaldotrip.domain.user.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import com.b6.mypaldotrip.domain.user.CommonTest;
+import com.b6.mypaldotrip.domain.user.exception.EmailErrorCode;
 import com.b6.mypaldotrip.domain.user.store.entity.EmailAuth;
+import com.b6.mypaldotrip.global.exception.GlobalException;
 import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -36,5 +41,33 @@ class EmailUtilTest implements CommonTest {
         String email =emailUtil.sendMessage(TEST_EMAIL,"테스트 인증 코드입니다.");
         //then
         assertThat(email).isEqualTo(TEST_EMAIL);
+    }
+    @Nested
+    @DisplayName("코드 검증 테스트")
+    class 코드검증{
+        @Test
+        @DisplayName("코드 검증 테스트 성공")
+        void 코드검증1 (){
+            //given
+            EmailAuth emailAuth = EmailAuth.builder().email(TEST_EMAIL).code("testCode").build();
+            given(emailAuthService.findById(any())).willReturn(emailAuth);
+            //when
+            emailUtil.verifyCode(TEST_EMAIL,"testCode");
+            //then
+            verify(emailAuthService).successVerify(emailAuth);
+        }
+        @Test
+        @DisplayName("코드 검증 테스트 실패")
+        void 코드검증2 (){
+            //given
+            EmailAuth emailAuth = EmailAuth.builder().email(TEST_EMAIL).code("testCode").build();
+            given(emailAuthService.findById(any())).willReturn(emailAuth);
+            //when
+            var exception =assertThrows(GlobalException.class, () -> {
+                emailUtil.verifyCode(TEST_EMAIL, "wrong-code");
+            });
+            //then
+            assertThat(exception.getResultCode()).isEqualTo(EmailErrorCode.CODE_MISMATCH);
+        }
     }
 }
