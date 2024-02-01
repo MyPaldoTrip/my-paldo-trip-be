ff011471c31a3355854df12cb0dc3526dbc0bb33
yoooooungwoon <kim0310hwang@gmail.com>
2024-02-01 17:31:09 +0900

    [#144] feat : 날씨 api 적용

---
+++ src/main/java/com/b6/mypaldotrip/domain/weather/service/WeatherService.java
@@ -0,0 +1,98 @@
+package com.b6.mypaldotrip.domain.weather.service;
+
+import com.b6.mypaldotrip.domain.weather.controller.dto.CustomOpenWeatherReq;
+import com.b6.mypaldotrip.domain.weather.controller.dto.CustomOpenWeatherRes;
+import com.b6.mypaldotrip.domain.weather.controller.dto.OpenWeatherRes;
+import com.b6.mypaldotrip.domain.weather.controller.dto.OpenWeatherRes.WeatherItem;
+import com.b6.mypaldotrip.domain.weather.exception.WeatherErrorCode;
+import com.b6.mypaldotrip.domain.weather.store.CityMapping;
+import com.b6.mypaldotrip.global.exception.GlobalException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Slf4j
+@Service
+public class WeatherService {
+
+    @Value("${weather.api-key}")
+    String apiKey;
+
+    // @Cacheable(value = "weatherCache", key = "{#root.methodName, #openWeatherReq.cityName}")
+    public OpenWeatherRes getOpenWeather(CustomOpenWeatherReq openWeatherReq) {
+
+        String changeCityName = CityMapping.convertToEnglish(openWeatherReq.cityName());
+
+        String url = "https://api.openweathermap.org/data/2.5/forecast";
+        WebClient webClient = WebClient.builder().baseUrl(url).build();
+
+        String apiUrl = "?q={cityName}&appid={APIkey}&units=metric";
+        String uriString =
+                UriComponentsBuilder.fromUriString(apiUrl)
+                        .buildAndExpand(changeCityName, apiKey)
+                        .toUriString();
+
+        OpenWeatherRes weatherResponse =
+                webClient.get().uri(uriString).retrieve().bodyToMono(OpenWeatherRes.class).block();
+
+        List<WeatherItem> weatherfilteredList = Collections.emptyList();
+
+        if (weatherResponse != null && weatherResponse.list() != null) {
+            // 3시에 해당하는 데이터만 필터링하여 응답
+            weatherfilteredList = weatherResponse.list().stream().toList();
+        } else {
+            throw new GlobalException(WeatherErrorCode.WEATHER_CHECK_FAIL);
+        }
+        return new OpenWeatherRes(weatherfilteredList);
+    }
+
+    public List<CustomOpenWeatherRes> processWeatherData(OpenWeatherRes weatherResponse) {
+        Map<String, CustomOpenWeatherRes> resultMap = new LinkedHashMap<>();
+
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        for (OpenWeatherRes.WeatherItem item : weatherResponse.list()) {
+            LocalDateTime dateTime = LocalDateTime.parse(item.date(), inputFormatter);
+            String formattedDate = dateTime.format(outputFormatter);
+
+            resultMap.compute(
+                    formattedDate,
+                    (key, existingValue) -> {
+                        if (existingValue == null) {
+                            return new CustomOpenWeatherRes(
+                                    formattedDate,
+                                    item.weather().get(0).main(),
+                                    item.main().temp_max(),
+                                    item.main().temp_min());
+                        } else {
+                            // 이미 있는 경우, 최고 기온과 최저 기온을 갱신
+                            double maxTemp =
+                                    Math.max(existingValue.max_temp(), item.main().temp_max());
+                            double minTemp =
+                                    Math.min(existingValue.min_temp(), item.main().temp_min());
+
+                            return new CustomOpenWeatherRes(
+                                    formattedDate, item.weather().get(0).main(), maxTemp, minTemp);
+                        }
+                    });
+        }
+
+        return new ArrayList<>(resultMap.values());
+    }
+
+    @CacheEvict(value = "weatherCache", allEntries = true)
+    @Scheduled(fixedRate = 6 * 60 * 60 * 1000) // 6시간마다 실행
+    public void deleteWeatherCache() {}
+}
---
+++ src/main/java/com/b6/mypaldotrip/domain/weather/store/CityMapping.java
@@ -0,0 +1,33 @@
+package com.b6.mypaldotrip.domain.weather.store;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum CityMapping {
+    SEOUL("서울", "seoul"),
+    BUSAN("부산", "busan"),
+    INCHEON("인천", "incheon"),
+    DAEGU("대구", "daegu"),
+    JEJU("제주도", "jeju"),
+    GWANGJU("광주", "Gwangju"),
+    DAEJEON("대전", "Daejeon "),
+    GYEONGGI("경기", "Gyeonggi-do"),
+    CHUNGNAM("충남", "Chungcheongnam-do"),
+    CHUNGBUK("충북", "Chungcheongbuk-do"),
+    JEOLLANAM("전라남도", "Jeollanam-do"),
+    JEOLLABUK("전라북도", "Jeollabuk-do"),
+    GYEONGSANGNAM("경상남도", "Gyeongsangnam-do"),
+    GYEONGSANGBUK("경상북도", "Gyeongsangbuk-do");
+
+    private final String beforeCityName;
+    private final String changeCityName;
+
+    public static String convertToEnglish(String koreanName) {
+        for (CityMapping city : values()) {
+            if (city.beforeCityName.equals(koreanName)) {
+                return city.changeCityName;
+            }
+        }
+        return koreanName; // 매핑이 없을 경우 그대로 반환
+    }
+}
