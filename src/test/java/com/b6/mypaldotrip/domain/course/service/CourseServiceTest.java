38541ea7561bdba68ac426cbdfcbd90d6a08669e
Sk1palong <cchs33@gmail.com>
2024-02-02 16:34:37 +0900

    [#142] test : CourseServiceTest - 조회

--- src/test/java/com/b6/mypaldotrip/domain/course/service/CourseServiceTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/course/service/CourseServiceTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 import com.b6.mypaldotrip.domain.city.service.CityService;
 import com.b6.mypaldotrip.domain.city.store.entity.CityEntity;
 import com.b6.mypaldotrip.domain.course.CourseTest;
 import com.b6.mypaldotrip.domain.course.controller.dto.request.CourseSaveReq;
+import com.b6.mypaldotrip.domain.course.controller.dto.request.CourseSearchReq;
+import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseGetRes;
+import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseListRes;
 import com.b6.mypaldotrip.domain.course.controller.dto.response.CourseSaveRes;
 import com.b6.mypaldotrip.domain.course.store.entity.CourseEntity;
 import com.b6.mypaldotrip.domain.course.store.repository.CourseRepository;
@@ -17,9 +21,11 @@ import com.b6.mypaldotrip.domain.trip.service.TripService;
 import com.b6.mypaldotrip.domain.trip.store.entity.TripEntity;
 import com.b6.mypaldotrip.domain.tripCourse.store.repository.TripCourseRepository;
 import com.b6.mypaldotrip.global.common.S3Provider;
+import com.b6.mypaldotrip.global.security.UserDetailsImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -27,6 +33,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
@@ -67,6 +77,78 @@ class CourseServiceTest implements CourseTest {
             // then
             assertEquals(TEST_COURSE_TITLE, res.title());
             assertEquals(TEST_COURSE_CONTENT, res.content());
+        }
+    }
+
+    @Nested
+    @DisplayName("코스 조회")
+    class 코스조회 {
+
+        @Test
+        @DisplayName("코스 목록 조회 - 로그인시")
+        void 코스목록조회() {
+            // given
+            CourseSearchReq req = CourseSearchReq.builder().build();
+
+            UserDetailsImpl userDetails = new UserDetailsImpl(TEST_USER);
+
+            List<CourseEntity> courseEntities = List.of(TEST_COURSE);
+
+            Pageable pageable = PageRequest.of(0, 10);
+            Page<CourseEntity> coursePage =
+                    new PageImpl<>(courseEntities, pageable, courseEntities.size());
+            when(courseRepository.getCourseListByDynamicConditions(
+                            any(), any(), any(), any(), any()))
+                    .thenReturn(coursePage);
+
+            // when
+            List<CourseListRes> res =
+                    courseService.getCourseListByDynamicConditions(0, 10, req, userDetails);
+
+            // then
+            assertEquals(courseEntities.size(), res.size());
+            assertEquals(TEST_COURSE.getTitle(), res.get(0).title());
+        }
+
+        @Test
+        @DisplayName("코스 목록 조회 - 비로그인시")
+        void 코스목록조회2() {
+            // given
+            CourseSearchReq req = CourseSearchReq.builder().build();
+
+            UserDetailsImpl userDetails = null;
+
+            List<CourseEntity> courseEntities = List.of(TEST_COURSE);
+
+            Pageable pageable = PageRequest.of(0, 10);
+            Page<CourseEntity> coursePage =
+                    new PageImpl<>(courseEntities, pageable, courseEntities.size());
+            when(courseRepository.getCourseListByDynamicConditions(
+                            any(), any(), any(), any(), any()))
+                    .thenReturn(coursePage);
+
+            // when
+            List<CourseListRes> res =
+                    courseService.getCourseListByDynamicConditions(0, 10, req, userDetails);
+
+            // then
+            assertEquals(courseEntities.size(), res.size());
+            assertEquals(TEST_COURSE.getTitle(), res.get(0).title());
+        }
+
+        @Test
+        @DisplayName("코스 단건 조회")
+        void 코스단건조회() {
+            // given
+            Long courseId = 1L;
+
+            when(courseRepository.findById(courseId)).thenReturn(Optional.ofNullable(TEST_COURSE));
+
+            // when
+            CourseGetRes res = courseService.getCourse(courseId);
+
+            // then
+            assertEquals(courseId, res.courseId());
         }
     }
 }
