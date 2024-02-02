c2707a652864f18e643c48d3c03435d09b737733
Sk1palong <cchs33@gmail.com>
2024-02-02 19:43:37 +0900

    [#142] fix : spotless

--- src/test/java/com/b6/mypaldotrip/domain/course/store/repository/CustomCourseRepositoryTest.java
+++ src/test/java/com/b6/mypaldotrip/domain/course/store/repository/CustomCourseRepositoryTest.java
@@ -31,14 +31,10 @@ import org.springframework.test.util.ReflectionTestUtils;
 @ActiveProfiles("test")
 class CustomCourseRepositoryTest implements CourseTest {
 
-    @Autowired
-    private CourseRepository courseRepository;
-    @Autowired
-    private CustomCourseRepository customCourseRepository;
-    @Autowired
-    private CityRepository cityRepository;
-    @Autowired
-    private UserRepository userRepository;
+    @Autowired private CourseRepository courseRepository;
+    @Autowired private CustomCourseRepository customCourseRepository;
+    @Autowired private CityRepository cityRepository;
+    @Autowired private UserRepository userRepository;
     private CourseEntity course1;
     private CourseEntity course2;
     private Pageable pageable;
@@ -58,8 +54,6 @@ class CustomCourseRepositoryTest implements CourseTest {
         courseRepository.save(course1);
         courseRepository.save(course2);
         pageable = PageRequest.of(0, 5);
-
-
     }
 
     @Nested
@@ -70,18 +64,23 @@ class CustomCourseRepositoryTest implements CourseTest {
         @DisplayName("도시별로 찾기")
         void 동적조회1() {
             // given
-            CourseSearchReq req = CourseSearchReq.builder()
-                .courseSort(CourseSort.MODIFIED)
-                .filterByCityName("cityName")
-                .filterByFollowing(false)
-                .build();
+            CourseSearchReq req =
+                    CourseSearchReq.builder()
+                            .courseSort(CourseSort.MODIFIED)
+                            .filterByCityName("cityName")
+                            .filterByFollowing(false)
+                            .build();
             // when
-            Page<CourseEntity> res = customCourseRepository.getCourseListByDynamicConditions(
-                pageable,
-                req.courseSort(), req, user1.getUserId(), req.filterByFollowing());
+            Page<CourseEntity> res =
+                    customCourseRepository.getCourseListByDynamicConditions(
+                            pageable,
+                            req.courseSort(),
+                            req,
+                            user1.getUserId(),
+                            req.filterByFollowing());
             // then
-            assertThat(res.getContent().get(0).getCityEntity().getCityName()).isEqualTo(
-                city1.getCityName());
+            assertThat(res.getContent().get(0).getCityEntity().getCityName())
+                    .isEqualTo(city1.getCityName());
             assertThat(res.getTotalElements()).isEqualTo(1);
         }
 
@@ -89,29 +88,36 @@ class CustomCourseRepositoryTest implements CourseTest {
         @DisplayName("팔로우한 유저만 보기")
         void 동적조회2() {
             // given
-            CourseSearchReq req = CourseSearchReq.builder()
-                .courseSort(CourseSort.MODIFIED)
-                .filterByFollowing(true)
-                .build();
-            ReflectionTestUtils.setField(user2, "followingList", List.of(
-                FollowingEntity.builder().user(user2).followingUser(user1).build()));
+            CourseSearchReq req =
+                    CourseSearchReq.builder()
+                            .courseSort(CourseSort.MODIFIED)
+                            .filterByFollowing(true)
+                            .build();
+            ReflectionTestUtils.setField(
+                    user2,
+                    "followingList",
+                    List.of(FollowingEntity.builder().user(user2).followingUser(user1).build()));
             // when
-            Page<CourseEntity> res = customCourseRepository.getCourseListByDynamicConditions(
-                pageable, req.courseSort(), req, user2.getUserId(), req.filterByFollowing());
+            Page<CourseEntity> res =
+                    customCourseRepository.getCourseListByDynamicConditions(
+                            pageable,
+                            req.courseSort(),
+                            req,
+                            user2.getUserId(),
+                            req.filterByFollowing());
             // then
-            assertThat(user2.getFollowingList().get(0).getFollowingUser()).isEqualTo(
-                res.getContent().get(0).getUserEntity());
+            assertThat(user2.getFollowingList().get(0).getFollowingUser())
+                    .isEqualTo(res.getContent().get(0).getUserEntity());
             assertThat(res.getTotalElements()).isEqualTo(1);
         }
-
     }
 
     private UserEntity setUser(Long n) {
         return UserEntity.builder()
-            .email("user" + n + "@gmail.com")
-            .username("user" + n)
-            .password(String.valueOf(n))
-            .age(n)
-            .build();
+                .email("user" + n + "@gmail.com")
+                .username("user" + n)
+                .password(String.valueOf(n))
+                .age(n)
+                .build();
     }
 }
