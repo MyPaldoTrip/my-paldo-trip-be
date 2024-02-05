c39bd188c4f2a0cde2deeac40fcf43d086debca7
jimyung0418 <wjdwlaud0418@naver.com>
2024-02-05 20:59:09 +0900

    [#176] feat : security설정에 JwtAuthenticationEntryPoint 추가

--- src/main/java/com/b6/mypaldotrip/global/config/WebSecurityConfig.java
+++ src/main/java/com/b6/mypaldotrip/global/config/WebSecurityConfig.java
@@ -1,13 +1,11 @@
 package com.b6.mypaldotrip.global.config;
 
-import com.b6.mypaldotrip.global.security.JwtAuthenticationFilter;
-import com.b6.mypaldotrip.global.security.JwtAuthorizationFilter;
-import com.b6.mypaldotrip.global.security.JwtUtil;
-import com.b6.mypaldotrip.global.security.UserDetailsServiceImpl;
+import com.b6.mypaldotrip.global.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -42,6 +40,11 @@ public class WebSecurityConfig {
         JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtUtil, versionConfig);
         filter.setAuthenticationManager(authenticationConfiguration.getAuthenticationManager());
         return filter;
+    }
+
+    @Bean
+    public JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
     }
 
     @Bean
@@ -93,6 +96,9 @@ public class WebSecurityConfig {
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated());
+
+        http.exceptionHandling(e -> e.authenticationEntryPoint(jwtAuthenticationEntryPoint()));
+
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
